6afb56b1752b88270018abc6391ba670a278bf34
Chris de Vreeze <chris.de.vreeze@caiway.net>
2017-02-05 19:00:57 +0100

    Added program showing concept labels

---
+++ src/main/scala/eu/cdevreeze/tqa/console/ShowLabels.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2011-2017 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.tqa.console
+
+import java.io.File
+import java.net.URI
+import java.util.logging.Logger
+
+import scala.collection.immutable
+import scala.reflect.classTag
+
+import eu.cdevreeze.tqa.backingelem.DocumentBuilder
+import eu.cdevreeze.tqa.backingelem.indexed.IndexedDocumentBuilder
+import eu.cdevreeze.tqa.backingelem.nodeinfo.SaxonDocumentBuilder
+import eu.cdevreeze.tqa.relationship.ConceptLabelRelationship
+import eu.cdevreeze.tqa.relationship.DefaultRelationshipFactory
+import eu.cdevreeze.tqa.taxonomybuilder.DefaultDtsCollector
+import eu.cdevreeze.tqa.taxonomybuilder.TaxonomyBuilder
+import eu.cdevreeze.yaidom.core.EName
+import eu.cdevreeze.yaidom.parse.DocumentParserUsingStax
+import net.sf.saxon.s9api.Processor
+
+/**
+ * Program that shows concept labels in a given taxonomy. One use case is finding translations for certain
+ * terms in the given taxonomy, provided there are labels in these desired languages.
+ *
+ * @author Chris de Vreeze
+ */
+object ShowLabels {
+
+  private val logger = Logger.getGlobal
+
+  def main(args: Array[String]): Unit = {
+    require(args.size >= 2, s"Usage: ShowLabels <taxo root dir> <entrypoint URI 1> ...")
+    val rootDir = new File(args(0))
+    require(rootDir.isDirectory, s"Not a directory: $rootDir")
+
+    val entrypointUris = args.drop(1).map(u => URI.create(u)).toSet
+
+    val useSaxon = System.getProperty("useSaxon", "false").toBoolean
+
+    val documentBuilder = getDocumentBuilder(useSaxon, rootDir)
+    val documentCollector = DefaultDtsCollector(entrypointUris)
+
+    val lenient = System.getProperty("lenient", "false").toBoolean
+
+    val relationshipFactory =
+      if (lenient) DefaultRelationshipFactory.LenientInstance else DefaultRelationshipFactory.StrictInstance
+
+    val taxoBuilder =
+      TaxonomyBuilder.
+        withDocumentBuilder(documentBuilder).
+        withDocumentCollector(documentCollector).
+        withRelationshipFactory(relationshipFactory)
+
+    logger.info(s"Starting building the DTS with entrypoint(s) ${entrypointUris.mkString(", ")}")
+
+    val basicTaxo = taxoBuilder.build()
+
+    val rootElems = basicTaxo.taxonomyBase.rootElems
+
+    val conceptLabelRelationships =
+      basicTaxo.findAllStandardRelationshipsOfType(classTag[ConceptLabelRelationship])
+
+    logger.info(s"The taxonomy has ${rootElems.size} taxonomy root elements")
+    logger.info(s"The taxonomy has ${basicTaxo.relationships.size} relationships")
+    logger.info(s"The taxonomy has ${conceptLabelRelationships.size} concept-label relationships")
+
+    val conceptLabelsByConcept: Map[EName, immutable.IndexedSeq[ConceptLabelRelationship]] =
+      conceptLabelRelationships.groupBy(_.sourceConceptEName)
+
+    val concepts = conceptLabelsByConcept.keySet.toIndexedSeq.sortBy(_.toString)
+
+    concepts foreach { concept =>
+      val conceptLabels = conceptLabelsByConcept.getOrElse(concept, Vector())
+
+      println(s"Concept: $concept")
+
+      conceptLabels foreach { rel =>
+        println(s"\tLabel (role: ${rel.resourceRole}, language: ${rel.language}): ${rel.resource.text}")
+      }
+    }
+
+    logger.info("Ready")
+  }
+
+  private def uriToLocalUri(uri: URI, rootDir: File): URI = {
+    // Not robust
+    val relativePath = uri.getScheme match {
+      case "http"  => uri.toString.drop("http://".size)
+      case "https" => uri.toString.drop("https://".size)
+      case _       => sys.error(s"Unexpected URI $uri")
+    }
+
+    val f = new File(rootDir, relativePath.dropWhile(_ == '/'))
+    f.toURI
+  }
+
+  private def getDocumentBuilder(useSaxon: Boolean, rootDir: File): DocumentBuilder = {
+    if (useSaxon) {
+      val processor = new Processor(false)
+
+      new SaxonDocumentBuilder(processor.newDocumentBuilder(), uriToLocalUri(_, rootDir))
+    } else {
+      new IndexedDocumentBuilder(DocumentParserUsingStax.newInstance(), uriToLocalUri(_, rootDir))
+    }
+  }
+}
