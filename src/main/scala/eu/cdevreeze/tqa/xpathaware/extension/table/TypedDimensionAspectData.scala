9207c7a7822884dc68a36c312adec82cdba912f6
Chris de Vreeze <chris.de.vreeze@inology.nl>
2017-05-30 09:30:18 +0200

    Added XPath-aware wrapper around TypedDimensionAspect

---
+++ src/main/scala/eu/cdevreeze/tqa/xpathaware/extension/table/TypedDimensionAspectData.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011-2017 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.tqa.xpathaware.extension.table
+
+import eu.cdevreeze.tqa.extension.table.dom.TypedDimensionAspect
+import eu.cdevreeze.tqa.xpath.XPathEvaluator
+import eu.cdevreeze.yaidom.core.EName
+import eu.cdevreeze.yaidom.queryapi.BackingElemApi
+
+/**
+ * Wrapper around an TypedDimensionAspect, which can extract the relevant data by evaluating XPath where needed.
+ *
+ * @author Chris de Vreeze
+ */
+final class TypedDimensionAspectData(val dimensionAspect: TypedDimensionAspect) {
+
+  /**
+   * Returns the dimension as EName, by calling `dimensionAspect.dimension`.
+   */
+  def dimensionName: EName = dimensionAspect.dimension
+
+  // Below, make sure that the passed XPathEvaluator knows about the needed namespace bindings in the XPath expressions.
+
+  def valueOption(implicit xpathEvaluator: XPathEvaluator): Option[BackingElemApi] = {
+    dimensionAspect.valueElemOption.map(_.underlyingElem.backingElem.findAllChildElems.head) orElse {
+      dimensionAspect.xpathElemOption.map(_.expr) map { expr =>
+        xpathEvaluator.evaluateAsBackingElem(xpathEvaluator.toXPathExpression(expr.xpathExpression), None)
+      }
+    }
+  }
+}
