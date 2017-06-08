450669884cde0e6a894e72f7fb4946c40f054f6f
Chris de Vreeze <chris.de.vreeze@inology.nl>
2017-06-08 09:07:27 +0200

    Adapted method InterConceptRelationship.isFollowedByTypeOf

--- src/main/scala/eu/cdevreeze/tqa/relationship/relationship.scala
+++ src/main/scala/eu/cdevreeze/tqa/relationship/relationship.scala
@@ -192,8 +192,8 @@ sealed abstract class InterConceptRelationship(
    * It is used by method followedBy.
    */
   protected def isFollowedByTypeOf(rel: InterConceptRelationship): Boolean = {
-    (this.baseSetKey.arcEName == rel.baseSetKey.arcEName) &&
-      (this.baseSetKey.arcrole == rel.baseSetKey.arcrole)
+    // TODO Refactor method isFollowedBy, and rename this method
+    this.baseSetKey == rel.baseSetKey
   }
 }
 
