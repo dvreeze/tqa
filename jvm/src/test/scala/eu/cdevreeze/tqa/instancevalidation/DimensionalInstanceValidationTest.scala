095567fcf9cb8b44cd386757c9e81f6f9d89ce4a
Chris de Vreeze <chris.de.vreeze@caiway.net>
2017-12-13 23:48:03 +0100

    Parsing from ZIP file in one test case

--- jvm/src/test/scala/eu/cdevreeze/tqa/instancevalidation/DimensionalInstanceValidationTest.scala
+++ jvm/src/test/scala/eu/cdevreeze/tqa/instancevalidation/DimensionalInstanceValidationTest.scala
@@ -28,7 +28,8 @@ import eu.cdevreeze.tqa.base.common.ContextElement
 import eu.cdevreeze.tqa.base.relationship.DefaultRelationshipFactory
 import eu.cdevreeze.tqa.base.taxonomybuilder.DefaultDtsCollector
 import eu.cdevreeze.tqa.base.taxonomybuilder.TaxonomyBuilder
-import eu.cdevreeze.tqa.docbuilder.jvm.UriConverters
+import eu.cdevreeze.tqa.docbuilder.SimpleCatalog
+import eu.cdevreeze.tqa.docbuilder.jvm.PartialUriResolvers
 import eu.cdevreeze.tqa.docbuilder.jvm.UriResolvers
 import eu.cdevreeze.tqa.instance.XbrlInstance
 import eu.cdevreeze.yaidom.core.EName
@@ -423,10 +424,11 @@ class DimensionalInstanceValidationTest extends FunSuite {
   }
 
   private def makeTestInstance(relativeDocPath: String): XbrlInstance = {
-    val rootDir = new File(classOf[DimensionalInstanceValidationTest].getResource("/conf-suite-dim").toURI)
-    val docFile = new File(rootDir, relativeDocPath)
+    // We expect no spaces in the path, so we can create a relative URI from it.
 
-    XbrlInstance(docBuilder.build(docFile.toURI))
+    val uri = dummyUriPrefix.resolve(relativeDocPath)
+
+    XbrlInstance(docBuilder.build(uri))
   }
 
   private def doMakeValidator(entryPointUris: Set[URI], doResolveProhibitionAndOverriding: Boolean): DimensionalValidator = {
@@ -441,6 +443,7 @@ class DimensionalInstanceValidationTest extends FunSuite {
         withRelationshipFactory(relationshipFactory)
 
     val basicTaxo = taxoBuilder.build(entryPointUris)
+
     val effectiveTaxo =
       if (doResolveProhibitionAndOverriding) {
         basicTaxo.resolveProhibitionAndOverriding(relationshipFactory)
@@ -453,21 +456,24 @@ class DimensionalInstanceValidationTest extends FunSuite {
 
   private val processor = new Processor(false)
 
-  private val docBuilder = {
+  private val dummyUriPrefix: URI = URI.create("http://www.example.com/")
+
+  private val docBuilder: SaxonDocumentBuilder = {
     val otherRootDir = new File(classOf[DimensionalInstanceValidationTest].getResource("/xbrl-and-w3").toURI)
+    val zipFile = new File(classOf[DimensionalInstanceValidationTest].getResource("/xdt-conf-cr4-2009-10-06.zip").toURI)
 
-    val xbrlAndW3UriConverter = UriConverters.fromLocalMirrorRootDirectory(otherRootDir)
+    val xbrlAndW3UriPartialResolver = PartialUriResolvers.fromLocalMirrorRootDirectory(otherRootDir)
 
-    def convertUri(uri: URI): URI = {
-      if (uri.getScheme == "http" || uri.getScheme == "https") {
-        xbrlAndW3UriConverter(uri)
-      } else {
-        uri
-      }
-    }
+    val catalog =
+      SimpleCatalog(
+        None,
+        Vector(SimpleCatalog.UriRewrite(None, dummyUriPrefix.toString, "")))
+
+    val zipFilePartialResolver = PartialUriResolvers.forZipFileUsingCatalog(zipFile, catalog)
 
     SaxonDocumentBuilder(
       processor.newDocumentBuilder(),
-      UriResolvers.fromUriConverter(convertUri _))
+      UriResolvers.fromPartialUriResolversWithFallback(
+        Vector(zipFilePartialResolver, xbrlAndW3UriPartialResolver)))
   }
 }
